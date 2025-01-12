ca49f28e944bc267e74afb727dbfe79d1bb0efe3
Nada Hasni <nada.hasni@edi-indonesia.co.id>
2025-01-12 17:05:13 +0700

    feat: footer dengan artikel semauku

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header className="bg-purple-700 w-full p-4">
+      <header className="bg-blue-700 w-full p-4">
         <div className="container mx-auto">
-          <h1 className="text-white text-xl">Aplikasi Mencoba GITFLOW</h1>
+          <h1 className="text-white text-xl">Aplikasi Footer</h1>
         </div>
       </header>
+      <main className="h-screen">
+        <article>
+          <h2>Ini Artikel Terserah Mauku</h2>
+        </article>
+      </main>
+      <footer className="p-4 bg-blue-700">
+        <div className="container mx-auto">
+          <p className="text-white text-center">Copyright @kodeku</p>
+        </div>
+      </footer>
     </>
   );
 }
