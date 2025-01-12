9c70c80d4cc2b3992c0f556a23a3ffe60e6442a5
Nada Hasni <nada.hasni@edi-indonesia.co.id>
2025-01-12 16:47:42 +0700

    feat: header navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="bg-slate-600 w-full p-4">
+        <div className="container mx-auto">
+          <h1 className="text-white text-xl">Aplikasi Mencoba GITFLOW</h1>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default App;
